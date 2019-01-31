aeb001e4dd1805020b30f98a8eafede3be579868
zhehuiTong <564860814@qq.com>
2019-01-31 15:46:11 +0800

    scrooll
    无限列表

---
+++ CollisionDetection.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionDetection : MonoBehaviour {
+
+    private bool isUP = false;
+    private int loopListCount;
+    private int itemPrefadHeigh;
+    private int itemSpacing;
+
+    public bool IsUP
+    {
+        get
+        {
+            return isUP;
+        }
+
+        set
+        {
+            isUP = value;
+        }
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "ScrollItem")
+        {
+            Image currentItem = collision.collider.GetComponent<Image>();
+            Vector2 currentAnchoredPosition= currentItem.rectTransform.anchoredPosition;
+            int itemHeigh = -(itemPrefadHeigh * loopListCount + itemSpacing * loopListCount);
+            if (isUP)
+            {
+                currentItem.rectTransform.anchoredPosition = new Vector2(0, currentAnchoredPosition.y + itemHeigh);
+                int index = int.Parse(currentItem.gameObject.name.Remove(0,4));
+                currentItem.name = "Item" + (index +loopListCount);
+
+            }
+            else
+            {
+                int index = int.Parse(currentItem.gameObject.name.Remove(0, 4));
+                currentItem.rectTransform.anchoredPosition = new Vector2(0, currentAnchoredPosition.y - itemHeigh);
+                currentItem.name = "Item" + (index - loopListCount);
+            }
+        }
+    }
+
+    public void Init(int listCount,int itemHeight,int spacing)
+    {
+        this.loopListCount = listCount;
+        this.itemPrefadHeigh = itemHeight;
+        this.itemSpacing = spacing;
+    }
+
+}
---
+++ ScrollList.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ScrollList : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler {
+
+    [SerializeField]
+    private int listCount;
+    [SerializeField]
+    private int itemSpacing;
+    [SerializeField]
+    private int loopListCount;
+    [SerializeField]
+    private RectTransform imageScrolRect;
+    [SerializeField]
+    private RectTransform imageItemPrefad;
+    [SerializeField]
+    private GameObject collisionArea;
+    private float beginDragPosition;
+    private float EndDragPosition;
+
+    // Use this for initialization
+    void Start () {
+        InitScrollList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        beginDragPosition = imageScrolRect.anchoredPosition.y;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        EndDragPosition = imageScrolRect.anchoredPosition.y;
+        JudgeUPAndDown();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        EndDragPosition = imageScrolRect.anchoredPosition.y;
+        JudgeUPAndDown();
+    }
+
+    private void InitScrollList()
+    {
+        InitImageScrolRect();
+        InitCollisionArea();
+        InitCreatItem();
+        collisionArea.GetComponent<CollisionDetection>().Init(loopListCount, (int)imageItemPrefad.sizeDelta.y, itemSpacing);
+    }
+
+    private void InitImageScrolRect()
+    {
+        int rectHeight = (int)imageItemPrefad.sizeDelta.y*listCount+itemSpacing*(listCount-1);
+        imageScrolRect.sizeDelta = new Vector2(500, rectHeight);
+        
+    }
+
+    private void InitCollisionArea()
+    {
+        int collisionHeight = (int)imageItemPrefad.sizeDelta.y * loopListCount + itemSpacing * (loopListCount - 1);
+        collisionArea.GetComponent<Image>().rectTransform.sizeDelta = new Vector2(500, collisionHeight);
+        BoxCollider2D collider2D = collisionArea.GetComponent<BoxCollider2D>();
+        //collider2D.offset = new Vector2(0, -(collisionHeight / 2));
+        collider2D.size = new Vector2(500, collisionHeight);
+    }
+
+    private void InitCreatItem()
+    {
+        for(int i = 0; i < loopListCount; i++)
+        {
+            GameObject itemGame = Instantiate(imageItemPrefad.gameObject);
+            itemGame.transform.SetParent(imageScrolRect);
+            int itemHeight = (int)imageItemPrefad.sizeDelta.y * i + itemSpacing * i;
+            itemGame.GetComponent<Image>().rectTransform.anchoredPosition = new Vector2(0, -itemHeight);
+            itemGame.name = "Item" + i;
+        }
+    }
+
+    private void JudgeUPAndDown()
+    {
+        float result = beginDragPosition - EndDragPosition;
+        if (result >= 0)
+        {
+            Debug.Log("下");
+            collisionArea.GetComponent<CollisionDetection>().IsUP = false;
+        }
+        else
+        {
+            Debug.Log("上");
+            collisionArea.GetComponent<CollisionDetection>().IsUP = true;
+        }
+    }
+
+   
+}
